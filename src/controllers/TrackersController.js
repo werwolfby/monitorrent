8402560de7479c3c40cf13a0e3d7d6ef8efcf440
werwolf.by <werwolf.by@gmail.com>
2015-07-29 00:36:34 +0300

    [-] Remove unused code.

--- src/controllers/TrackersController.js
+++ src/controllers/TrackersController.js
@@ -1,4 +1,4 @@
-app.controller('TrackersController', function ($scope, TrackersService, $mdToast, $compile, DynamicForm) {
+app.controller('TrackersController', function ($scope, TrackersService, $mdToast) {
     $scope.save = function (client, credentials) {
         TrackersService.save(client, credentials).then(function () {
             $mdToast.show(
@@ -38,53 +38,4 @@ app.controller('TrackersController', function ($scope, TrackersService, $mdToast
             $scope.trackers.push(tracker);
         });
     });
-
-    $scope.testData = [
-        {
-            "type": "row",
-            "content": [{
-                "type": "text",
-                "model": "username1",
-                "label": "Username",
-                "flex": 45
-            }, {
-                "type": "password",
-                "model": "password1",
-                "label": "Password",
-                "flex": 45
-            }, {
-                "type": "text",
-                "model": "password",
-                "label": "Test",
-                "flex": 10
-            }]
-        },{
-            "type": "row",
-            "content": [{
-                "type": "text",
-                "model": "username2",
-                "label": "Username",
-                "flex": 45
-            }, {
-                "type": "password",
-                "model": "password2",
-                "label": "Password",
-                "flex": 45
-            }, {
-                "type": "select",
-                "model": "quality",
-                "label": "Default Quality",
-                "options": ["SD", "720p", "1080p"],
-                "flex": 10
-            }]
-        }];
-
-    $scope.testCallback = function() {
-        alert($scope.test);
-    };
-
-    /*var elem = angular.element( document.querySelector( '#testtitle' ) );
-    var form = DynamicForm($scope, data, 'test');
-    elem.append(form);*/
-
 });
