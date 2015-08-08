aba053743944ff2a508e5ec5f0357cbc6a85c016
werwolf.by <werwolf.by@gmail.com>
2015-08-08 14:40:17 +0300

    [*] Extract lostfilm helper class to remove sensitive data from vcr cassettes.

--- monitorrent/tests/__init__.py
+++ monitorrent/tests/__init__.py
@@ -14,8 +14,9 @@ def use_vcr(func=None, **kwargs):
     if func is None:
         # When called with kwargs, e.g. @use_vcr(inject_cassette=True)
         return functools.partial(use_vcr, **kwargs)
-    module = func.__module__.split('tests.')[-1]
-    class_name = inspect.stack()[1][3]
-    cassette_name = '.'.join([module, class_name, func.__name__])
-    kwargs.setdefault('path', cassette_name)
+    if 'path' not in kwargs:
+        module = func.__module__.split('tests.')[-1]
+        class_name = inspect.stack()[1][3]
+        cassette_name = '.'.join([module, class_name, func.__name__])
+        kwargs.setdefault('path', cassette_name)
     return test_vcr.use_cassette(**kwargs)(func)
