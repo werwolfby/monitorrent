f7acf4412749bc89c1a68526317b857a13b2ccc0
werwolf.by <werwolf.by@gmail.com>
2015-08-10 00:24:22 +0300

    [+] Add change password form.

--- src/controllers/SettingsController.js
+++ src/controllers/SettingsController.js
@@ -1,2 +1,14 @@
-app.controller('SettingsController', function ($scope) {
+app.controller('SettingsController', function ($scope, $http) {
+    $scope.verify_new_password = function () {
+    };
+
+    $scope.changePassword = function () {
+        if ($scope.new_password != $scope.retry_password) {
+            return;
+        }
+        var settings = {'old_password': $scope.old_password, 'new_password': $scope.new_password};
+        $http.post('/api/settings', settings)
+            .success(function (data) {
+            });
+    };
 });
