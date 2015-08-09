cc564837dcda6227dbc129c6d6baa17d3ef3ba51
maxvoitas <maxvoitas@gmail.com>
2015-08-10 00:50:46 +0300

    Updated color scheme. Icons were added. New layout for torrents view (WIP). Basic responsive design support (WIP). Current version added to header.

--- src/controllers/TorrentsController.js
+++ src/controllers/TorrentsController.js
@@ -5,40 +5,6 @@ app.controller('TorrentsController', function ($scope, TopicsService, $mdDialog,
         });
     }
 
-    function AddTorrentDialogController($scope, $mdDialog) {
-        $scope.cancel = function() {
-            $mdDialog.cancel();
-        };
-        $scope.add = function() {
-            TopicsService.add($scope.url, $scope.settings).then(function() {
-                $mdDialog.hide();
-            });
-        };
-        $scope.parseUrl = function () {
-            $scope.isloading = true;
-            $scope.isloaded = false;
-            $scope.disabled = true;
-            TopicsService.parseUrl($scope.url).success(function (data) {
-                $scope.form = data.form;
-                $scope.settings = data.settings;
-                $scope.isError = false;
-                $scope.isloading = false;
-                $scope.isloaded = true;
-                $scope.disabled = false;
-            }).error(function () {
-                $scope.title = "Error";
-                $scope.isError = true;
-                $scope.isloading = false;
-                $scope.isloaded = true;
-                $scope.disabled = true;
-            });
-        };
-        $scope.url = "";
-        $scope.disabled = true;
-        $scope.isloading = false;
-        $scope.isloaded = false;
-    }
-
     function EditTorrentDialogController($scope, $mdDialog, id) {
         $scope.cancel = function() {
             $mdDialog.cancel();
@@ -79,22 +45,10 @@ app.controller('TorrentsController', function ($scope, TopicsService, $mdDialog,
             updateTorrents();
         });
     };
-
-    $scope.showAddTorrentDialog = function (ev) {
-        $mdDialog.show({
-            controller: AddTorrentDialogController,
-            templateUrl: 'views/add-torrent-dialog.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            locals: {
-                isEdit: false
-            }
-        }).then(function() {
-            updateTorrents();
-        }, function() {
-            updateTorrents();
-        });
-    };
+    
+    $scope.$on('mt-torrent-added', function() {
+       updateTorrents(); 
+    });
 
     updateTorrents();
 });
