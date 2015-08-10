64e0cb9f6bb86d0227e35048cbef36f7ec6c1728
werwolf.by <werwolf.by@gmail.com>
2015-08-11 01:29:38 +0300

    [+] Implement authentication ui.

--- src/controllers/SettingsController.js
+++ src/controllers/SettingsController.js
@@ -1,14 +1,30 @@
 app.controller('SettingsController', function ($scope, $http) {
     $scope.verify_new_password = function () {
     };
+    $scope.change_password = {};
+    $scope.authentication = {};
 
     $scope.updatePassword = function () {
-        if ($scope.new_password != $scope.retry_password) {
+        if ($scope.change_password.new_password != $scope.change_password.retry_password) {
             return;
         }
-        var settings = {'old_password': $scope.old_password, 'new_password': $scope.new_password};
+        var settings = {'old_password': $scope.change_password.old_password, 'new_password': $scope.change_password.new_password};
         $http.put('/api/settings/password', settings)
             .success(function (data) {
             });
+    };
+
+    $http.get('/api/settings/authentication').success(function (data) {
+        $scope.authentication.is_authentication_enabled = data.is_authentication_enabled;
+    });
+
+    $scope.setAuthentication = function () {
+        var settings = {
+            'password': $scope.authentication.password,
+            'is_authentication_enabled': $scope.authentication.is_authentication_enabled
+        };
+        $http.put('/api/settings/authentication', settings)
+            .success(function (data) {
+        });
     };
 });
