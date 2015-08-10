d4b755420332dd55d1660ff886887288da8e1d89
werwolf.by <werwolf.by@gmail.com>
2015-08-11 02:14:41 +0300

    [+] Display toasts and backend errors.

--- src/controllers/SettingsController.js
+++ src/controllers/SettingsController.js
@@ -1,8 +1,9 @@
-app.controller('SettingsController', function ($scope, $http) {
-    $scope.verify_new_password = function () {
+app.controller('SettingsController', function ($scope, $http, $mdToast) {
+    $scope.remove_backend_error = function (form, elem) {
+        elem.$setValidity('backend', true);
     };
-    $scope.change_password = {};
-    $scope.authentication = {};
+    $scope.change_password = {'backend_messages': {}};
+    $scope.authentication = {'backend_messages': {}};
 
     $scope.updatePassword = function () {
         if ($scope.change_password.new_password != $scope.change_password.retry_password) {
@@ -11,6 +12,15 @@ app.controller('SettingsController', function ($scope, $http) {
         var settings = {'old_password': $scope.change_password.old_password, 'new_password': $scope.change_password.new_password};
         $http.put('/api/settings/password', settings)
             .success(function (data) {
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content('Password was changed successfully')
+                        .position('right top')
+                        .hideDelay(3000)
+                );
+            }).error(function (data) {
+                $scope.change_password.backend_messages[data.param] = data.message;
+                $scope.changePassword[data.param].$setValidity('backend', false);
             });
     };
 
@@ -26,6 +36,15 @@ app.controller('SettingsController', function ($scope, $http) {
         $http.put('/api/settings/authentication', settings)
             .success(function (data) {
                 $scope.$emit('authentication.changed');
-        });
+                $mdToast.show(
+                    $mdToast.simple()
+                        .content('Authentication settings changed successfully')
+                        .position('right top')
+                        .hideDelay(3000)
+                );
+        }).error(function (data) {
+                $scope.authentication.backend_messages[data.param] = data.message;
+                $scope.enableDisableAuthentication[data.param].$setValidity('backend', false);
+            });
     };
 });
