24c3afc4b69f6829c49502270ba65cd0877984cd
werwolf.by <werwolf.by@gmail.com>
2015-08-20 19:57:14 +0300

    [+] Add single topic resource.

--- monitorrent/rest/topics.py
+++ monitorrent/rest/topics.py
@@ -3,11 +3,10 @@ from monitorrent.plugin_managers import TrackersManager
 
 
 # noinspection PyUnusedLocal
-class Topics(object):
+class TopicCollection(object):
     def __init__(self, tracker_manager):
         """
         :type tracker_manager: TrackersManager
-        :return:
         """
         self.tracker_manager = tracker_manager
 
@@ -28,7 +27,6 @@ class TopicParse(object):
     def __init__(self, tracker_manager):
         """
         :type tracker_manager: TrackersManager
-        :return:
         """
         self.tracker_manager = tracker_manager
 
@@ -38,3 +36,28 @@ class TopicParse(object):
         if not title:
             raise falcon.HTTPBadRequest('CantParse', 'Can\' parse url: \'{}\''.format(url))
         resp.json = title
+
+
+# noinspection PyUnusedLocal,PyShadowingBuiltins
+class Topic(object):
+    def __init__(self, tracker_manager):
+        """
+        :type tracker_manager: TrackersManager
+        """
+        self.tracker_manager = tracker_manager
+
+    def on_get(self, req, resp, id):
+        resp.json = self.tracker_manager.get_topic(id)
+
+    def on_put(self, req, resp, id):
+        settings = req.json
+        updated = self.tracker_manager.update_watch(id, settings)
+        if not updated:
+            raise falcon.HTTPNotFound(description='Can\'t update topic {}'.format(id))
+        resp.status = 204
+
+    def on_delete(self, req, resp, id):
+        deleted = self.tracker_manager.remove_topic(id)
+        if not deleted:
+            raise falcon.HTTPNotFound(description='Topic {} doesn\'t exist'.format(id))
+        resp.status = 204
--- server_falcon.py
+++ server_falcon.py
@@ -1,19 +1,12 @@
-import os
-from path import path
-import falcon
-import mimetypes
-import json
-import datetime
 from cherrypy import wsgiserver
 from monitorrent.engine import Logger, EngineRunner
 from monitorrent.db import init_db_engine, create_db, upgrade
 from monitorrent.plugin_managers import load_plugins, get_all_plugins, upgrades, TrackersManager, ClientsManager
 from monitorrent.plugins.trackers import TrackerPluginWithCredentialsBase
 from monitorrent.settings_manager import SettingsManager
-from functools import wraps
 from monitorrent.rest import create_api
 from monitorrent.rest.settings_authentication import SettingsAuthentication
-from monitorrent.rest.topics import Topics, TopicParse
+from monitorrent.rest.topics import TopicCollection, TopicParse, Topic
 
 init_db_engine("sqlite:///monitorrent.db", True)
 load_plugins()
@@ -26,7 +19,8 @@ settings_manager = SettingsManager()
 
 app = create_api('webapp')
 app.add_route('/api/settings/authentication', SettingsAuthentication(settings_manager))
-app.add_route('/api/topics', Topics(tracker_manager))
+app.add_route('/api/topics', TopicCollection(tracker_manager))
+app.add_route('/api/topics/{id}', Topic(tracker_manager))
 app.add_route('/api/parse', TopicParse(tracker_manager))
 
 if __name__ == '__main__':
