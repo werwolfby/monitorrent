76a15206eeb4d82e7d4d7ba6982d67e05863bed1
werwolf.by <werwolf.by@gmail.com>
2015-08-22 15:36:15 +0300

    [+] Implement setting is_authentication_enabled.

--- monitorrent/rest/settings_authentication.py
+++ monitorrent/rest/settings_authentication.py
@@ -1,3 +1,4 @@
+import falcon
 from monitorrent.settings_manager import SettingsManager
 
 
@@ -11,3 +12,21 @@ class SettingsAuthentication(object):
 
     def on_get(self, req, resp):
         resp.json = {'is_authentication_enabled': self.settings_manager.get_is_authentication_enabled()}
+
+    def on_put(self, req, resp):
+        if req.json is None:
+            raise falcon.HTTPBadRequest('BodyRequired', 'Expecting not empty JSON body')
+
+        if 'password' not in req.json or 'is_authentication_enabled' not in req.json:
+            raise falcon.HTTPBadRequest('ParamRequired',
+                                        '"password" and "is_authentication_enabled" properties are required')
+
+        password = req.json['password']
+        is_authentication_enabled = req.json['is_authentication_enabled']
+        if not isinstance(is_authentication_enabled, bool):
+            raise falcon.HTTPBadRequest('WrongValue', '"is_authentication_enabled" have to be bool')
+
+        if password != self.settings_manager.get_password():
+            raise falcon.HTTPBadRequest('WrongValue', '"password" is wrong')
+        self.settings_manager.set_is_authentication_enabled(is_authentication_enabled)
+        resp.status = falcon.HTTP_NO_CONTENT
