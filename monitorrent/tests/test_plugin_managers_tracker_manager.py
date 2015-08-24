bf3a96f37a6756cca255f658451d68e083887c88
werwolf.by <werwolf.by@gmail.com>
2015-08-25 02:00:29 +0300

    [+] Add tests for [plugin_manager]:[check_connection], [prepare_add_topic] and [add_topic].

--- monitorrent/tests/test_plugin_managers_tracker_manager.py
+++ monitorrent/tests/test_plugin_managers_tracker_manager.py
@@ -71,3 +71,157 @@ class TrackersManagerTest(TestCase):
         self.assertTrue(trackers_manager.set_settings(self.TRACKER2_PLUGIN_NAME, credentials2))
 
         update_credentials_mock2.assert_called_with(credentials2)
+
+    def test_check_connection(self):
+        tracker1 = self.Tracker1()
+        tracker2 = self.Tracker2()
+        trackers_manager = TrackersManager({
+            self.TRACKER1_PLUGIN_NAME: tracker1,
+            self.TRACKER2_PLUGIN_NAME: tracker2,
+        })
+
+        self.assertFalse(trackers_manager.check_connection(self.TRACKER1_PLUGIN_NAME))
+
+        verify_mock = MagicMock(return_value=True)
+        tracker2.verify = verify_mock
+
+        self.assertTrue(trackers_manager.check_connection(self.TRACKER2_PLUGIN_NAME))
+
+        verify_mock.assert_called_with()
+
+    def test_prepare_add_topic_1(self):
+        tracker1 = self.Tracker1()
+        tracker2 = self.Tracker2()
+        trackers_manager = TrackersManager({
+            self.TRACKER1_PLUGIN_NAME: tracker1,
+            self.TRACKER2_PLUGIN_NAME: tracker2,
+        })
+
+        parsed_url = {'display_name': "Some Name / Translated Name"}
+        prepare_add_topic_mock1 = MagicMock(return_value=parsed_url)
+        tracker1.prepare_add_topic = prepare_add_topic_mock1
+        result = trackers_manager.prepare_add_topic('http://tracker.com/1/')
+        self.assertIsNotNone(result)
+
+        prepare_add_topic_mock1.assert_called_with(('http://tracker.com/1/'))
+
+        self.assertEqual(result, {'form': TrackerPluginBase.topic_form, 'settings': parsed_url})
+
+    def test_prepare_add_topic_2(self):
+        tracker1 = self.Tracker1()
+        tracker2 = self.Tracker2()
+        trackers_manager = TrackersManager({
+            self.TRACKER1_PLUGIN_NAME: tracker1,
+            self.TRACKER2_PLUGIN_NAME: tracker2,
+        })
+
+        prepare_add_topic_mock1 = MagicMock(return_value=None)
+        tracker1.prepare_add_topic = prepare_add_topic_mock1
+
+        parsed_url = {'display_name': "Some Name / Translated Name"}
+        prepare_add_topic_mock2 = MagicMock(return_value=parsed_url)
+        tracker2.prepare_add_topic = prepare_add_topic_mock2
+
+        result = trackers_manager.prepare_add_topic('http://tracker.com/1/')
+        self.assertIsNotNone(result)
+
+        prepare_add_topic_mock1.assert_called_with('http://tracker.com/1/')
+        prepare_add_topic_mock2.assert_called_with('http://tracker.com/1/')
+
+        self.assertEqual(result, {'form': TrackerPluginBase.topic_form, 'settings': parsed_url})
+
+    def test_prepare_add_topic_3(self):
+        tracker1 = self.Tracker1()
+        tracker2 = self.Tracker2()
+        trackers_manager = TrackersManager({
+            self.TRACKER1_PLUGIN_NAME: tracker1,
+            self.TRACKER2_PLUGIN_NAME: tracker2,
+        })
+
+        prepare_add_topic_mock1 = MagicMock(return_value=None)
+        tracker1.prepare_add_topic = prepare_add_topic_mock1
+
+        prepare_add_topic_mock2 = MagicMock(return_value=None)
+        tracker2.prepare_add_topic = prepare_add_topic_mock2
+
+        result = trackers_manager.prepare_add_topic('http://tracker.com/1/')
+        self.assertIsNone(result)
+
+        prepare_add_topic_mock1.assert_called_with('http://tracker.com/1/')
+        prepare_add_topic_mock2.assert_called_with('http://tracker.com/1/')
+
+    def test_add_topic_1(self):
+        tracker1 = self.Tracker1()
+        tracker2 = self.Tracker2()
+        trackers_manager = TrackersManager({
+            self.TRACKER1_PLUGIN_NAME: tracker1,
+            self.TRACKER2_PLUGIN_NAME: tracker2,
+        })
+
+        can_parse_url_mock1 = MagicMock(return_value=True)
+        add_topic_mock1 = MagicMock(return_value=True)
+        tracker1.can_parse_url = can_parse_url_mock1
+        tracker1.add_topic = add_topic_mock1
+
+        params = {'display_name': "Some Name / Translated Name"}
+        url = 'http://tracker.com/1/'
+        self.assertTrue(trackers_manager.add_topic(url, params))
+
+        can_parse_url_mock1.assert_called_with(url)
+        add_topic_mock1.assert_called_with(url, params)
+
+    def test_add_topic_2(self):
+        tracker1 = self.Tracker1()
+        tracker2 = self.Tracker2()
+        trackers_manager = TrackersManager({
+            self.TRACKER1_PLUGIN_NAME: tracker1,
+            self.TRACKER2_PLUGIN_NAME: tracker2,
+        })
+
+        can_parse_url_mock1 = MagicMock(return_value=False)
+        add_topic_mock1 = MagicMock(return_value=False)
+        tracker1.can_parse_url = can_parse_url_mock1
+        tracker1.add_topic = add_topic_mock1
+
+        can_parse_url_mock2 = MagicMock(return_value=True)
+        add_topic_mock2 = MagicMock(return_value=True)
+        tracker2.can_parse_url = can_parse_url_mock2
+        tracker2.add_topic = add_topic_mock2
+
+        params = {'display_name': "Some Name / Translated Name"}
+        url = 'http://tracker.com/1/'
+        self.assertTrue(trackers_manager.add_topic(url, params))
+
+        can_parse_url_mock1.assert_called_with(url)
+        add_topic_mock1.assert_not_called()
+
+        can_parse_url_mock2.assert_called_with(url)
+        add_topic_mock2.assert_called_with(url, params)
+
+    def test_add_topic_3(self):
+        tracker1 = self.Tracker1()
+        tracker2 = self.Tracker2()
+        trackers_manager = TrackersManager({
+            self.TRACKER1_PLUGIN_NAME: tracker1,
+            self.TRACKER2_PLUGIN_NAME: tracker2,
+        })
+
+        can_parse_url_mock1 = MagicMock(return_value=False)
+        add_topic_mock1 = MagicMock(return_value=False)
+        tracker1.can_parse_url = can_parse_url_mock1
+        tracker1.add_topic = add_topic_mock1
+
+        can_parse_url_mock2 = MagicMock(return_value=False)
+        add_topic_mock2 = MagicMock(return_value=False)
+        tracker2.can_parse_url = can_parse_url_mock2
+        tracker2.add_topic = add_topic_mock2
+
+        params = {'display_name': "Some Name / Translated Name"}
+        url = 'http://tracker.com/1/'
+        self.assertFalse(trackers_manager.add_topic(url, params))
+
+        can_parse_url_mock1.assert_called_with(url)
+        add_topic_mock1.assert_not_called()
+
+        can_parse_url_mock2.assert_called_with(url)
+        add_topic_mock2.assert_not_called()
