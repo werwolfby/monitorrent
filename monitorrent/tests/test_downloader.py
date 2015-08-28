c5542017367e855ec8423f49cd9c267b2cda8c9c
werwolf.by <werwolf.by@gmail.com>
2015-08-29 01:43:55 +0300

    [+] Add tests for downloader

---
+++ monitorrent/tests/test_downloader.py
@@ -0,0 +1,33 @@
+import requests
+from ddt import ddt, data
+from monitorrent.tests import TestCase, use_vcr
+from monitorrent.utils.downloader import download
+
+
+@ddt
+class DownloaderTest(TestCase):
+    @data(False, True)
+    @use_vcr
+    def test_string_downloader(self, prepared=False):
+        url = 'http://google.com'
+        if prepared:
+            url = self.prepare_reques(url)
+        content, filename = download(url)
+
+        self.assertEqual(9766, len(content))
+        self.assertIsNone(filename)
+
+    @data(False, True)
+    @use_vcr
+    def test_file_downloader(self, prepared=False):
+        url = 'http://d.rutor.org/download/442959'
+        if prepared:
+            url = self.prepare_reques(url)
+        content, filename = download(url)
+
+        self.assertEqual(32814, len(content))
+        self.assertEqual(filename, '[rutor.org]Ray.Donovan_S03_720p.NewStudio.torrent')
+
+    def prepare_reques(self, url):
+        request = requests.Request('GET', url)
+        return request.prepare()
