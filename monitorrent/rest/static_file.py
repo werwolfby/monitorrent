e3a7800e83b1858cd06e19835f3b2e872c121df3
werwolf.by <werwolf.by@gmail.com>
2015-09-06 20:32:46 +0300

    [*] Return [Last-Modified], [ETag] and [Date] headers, return 404 in case of not existing file.

--- monitorrent/rest/static_file.py
+++ monitorrent/rest/static_file.py
@@ -1,7 +1,9 @@
-import mimetypes
 import os
+import time
+import mimetypes
 import falcon
 from monitorrent.rest import no_auth, AuthMiddleware
+from email.utils import formatdate
 
 
 @no_auth
@@ -20,7 +22,20 @@ class StaticFiles(object):
         file_path = filename or self.filename
         if self.folder:
             file_path = os.path.join(self.folder, file_path)
+        if not os.path.isfile(file_path):
+            raise falcon.HTTPNotFound(description='Requested page not found')
+
         mime_type, encoding = mimetypes.guess_type(file_path)
+        etag, last_modified = self._get_static_info(file_path)
+
         resp.content_type = mime_type
+        resp.set_header('Date', formatdate(time.time(), usegmt=True))
+        resp.set_header('ETag', etag)
+        resp.set_header('Last-Modified', last_modified)
         resp.stream_len = os.path.getsize(file_path)
         resp.stream = open(file_path, mode='rb')
+
+    @staticmethod
+    def _get_static_info(file_path):
+        mtime = os.stat(file_path).st_mtime
+        return str(mtime), formatdate(mtime, usegmt=True)
--- monitorrent/tests/test_api_static_file.py
+++ monitorrent/tests/test_api_static_file.py
@@ -1,7 +1,10 @@
 # coding=utf-8
+import os
+import time
+from collections import namedtuple
 import falcon
 from ddt import ddt, data
-from mock import patch, mock_open, MagicMock
+from mock import patch, mock_open, MagicMock, Mock
 from monitorrent.rest.static_file import StaticFiles
 from monitorrent.tests import RestTestBase
 
@@ -11,10 +14,16 @@ class TestStaticFiles(RestTestBase):
     @data(u'<HTML></HTML>', u'<HTML>Other text</HTML>')
     def test_static_index(self, index_text):
         # with not ascii chars in index_text test fails on travis-ci
-        m = mock_open(read_data=index_text)
-        getsize = MagicMock(return_value=len(index_text))
-        with patch("monitorrent.rest.static_file.open", m, create=True), \
-                patch("monitorrent.rest.static_file.os.path.getsize", getsize, create=True):
+        stat_class = namedtuple('stat_class', ['st_mtime'])
+        time_time = 1105022732.0
+        stat = stat_class(st_mtime=time_time)
+        with patch("monitorrent.rest.static_file.open", mock_open(read_data=index_text), create=True), \
+                patch("monitorrent.rest.static_file.os.path.isfile", Mock(return_value=True), create=True), \
+                patch("monitorrent.rest.static_file.os.stat", Mock(return_value=stat), create=True), \
+                patch("monitorrent.rest.static_file.mimetypes.guess_type", Mock(return_value=('text/html', 'utf-8')),
+                      create=True), \
+                patch("monitorrent.rest.static_file.os.path.getsize", MagicMock(return_value=len(index_text)),
+                      create=True):
             s = StaticFiles('folder', 'index.html', False)
             self.api.add_route('/index.html', s)
 
@@ -22,6 +31,8 @@ class TestStaticFiles(RestTestBase):
             self.assertEqual(self.srmock.status, falcon.HTTP_OK)
             self.assertEqual('text/html', self.srmock.headers_dict['content-type'])
             self.assertEqual(str(len(index_text)), self.srmock.headers_dict['content-length'])
+            self.assertEqual('Thu, 06 Jan 2005 14:45:32 GMT', self.srmock.headers_dict['last-modified'])
+            self.assertEqual('1105022732.0', self.srmock.headers_dict['etag'])
             self.assertEqual(unicode(body.next()), index_text)
 
     def test_redirect_to_login(self):
@@ -36,3 +47,12 @@ class TestStaticFiles(RestTestBase):
             self.simulate_request('/index.html')
             self.assertEqual(self.srmock.status, falcon.HTTP_FOUND)
             self.assertEqual('/login', self.srmock.headers_dict['location'])
+
+    def test_not_found(self):
+        if_file_mock = MagicMock(return_value=False)
+        with patch("monitorrent.rest.static_file.os.path.isfile", if_file_mock, create=True):
+            s = StaticFiles('folder', 'index.html', False)
+            self.api.add_route('/index.html', s)
+
+            self.simulate_request('/index.html')
+            self.assertEqual(self.srmock.status, falcon.HTTP_NOT_FOUND)
