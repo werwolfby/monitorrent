3b289677c90a29205f9ae1440e2e39d66699569c
werwolf.by <werwolf.by@gmail.com>
2015-10-24 00:41:14 +0300

    [+] Add infinite loading of logs.

--- src/controllers/logs/LogsController.js
+++ src/controllers/logs/LogsController.js
@@ -9,7 +9,21 @@ app.controller('LogsController', function ($scope, $http, $filter) {
     return "Execute from <i><b>" + from + "</b></i> till <i><b>" + till + "</b></i>";
   };
 
-  $http.get('/api/execute/logs?take=10').then(function (result) {
-    $scope.executes = result.data.data;
-  });
+  var PAGE_SIZE = 10;
+  var last_page = 0;
+  var length = 0;
+
+  function loadNextPage() {
+    if (length > 0 && last_page * PAGE_SIZE >= length)
+      return;
+    $http.get('/api/execute/logs?take=' + PAGE_SIZE + "&skip=" + (last_page * PAGE_SIZE)).then(function (result) {
+      for (var i = 0; i < result.data.data.length; i++) {
+        $scope.executes.push(result.data.data[i]);
+      }
+      length = result.data.count;
+    });
+    last_page++;
+  }
+
+  $scope.loadNextPage = loadNextPage;
 });
