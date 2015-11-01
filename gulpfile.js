9a527c8b54f7cf7c72f26f1f4d550e1c33da8315
werwolf.by <werwolf.by@gmail.com>
2015-11-01 09:28:26 +0300

    [+] Add release task to create zip archive.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
 var less = require('gulp-less');
 var preprocess = require('gulp-preprocess');
+var zip = require('gulp-zip');
 
 var pkg = require('./package.json');
 
@@ -15,13 +16,18 @@ var paths = {
   index_pages: ['./src/*.html'],
   statics: ['./src/**/*.html', './src/**/*.svg', './src/**/*.png', './src/favicon.ico', '!./src/*.html'],
   styles: ['./src/**/*.less'],
-  dest: './webapp'
+  dest: 'webapp',
+  release: 'dist'
 };
 
 gulp.task('default', ['clean', 'jshint', 'concat', 'copy', 'less', 'preprocess']);
 
 gulp.task('clean', function () {
   return del.sync([paths.dest]);
+});
+
+gulp.task('clean-release', function () {
+  return del.sync([paths.release]);
 });
 
 gulp.task('jshint', function () {
@@ -64,3 +70,24 @@ gulp.task('watch', ['default'], function () {
   gulp.watch(paths.styles,      ['less']);
   gulp.watch(paths.index_pages, ['preprocess']);
 });
+
+gulp.task('release', ['clean-release', 'copy-python', 'copy-webapp', 'copy-desc'], function () {
+  return gulp.src([paths.release + '/**/*.*'])
+    .pipe(zip(pkg.name + '-' + pkg.version + '.zip'))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('copy-python', function () {
+  return gulp.src(['./**/*.py', '!./monitorrent/tests/*.*', '!./monitorrent/tests_functional/*.*', '!./' + paths.release + '/**/*.py'])
+    .pipe(gulp.dest(paths.release));
+});
+
+gulp.task('copy-webapp', ['default'], function () {
+  return gulp.src([path.join(paths.dest, '**/*.*'), '!./**/*.map'])
+    .pipe(gulp.dest(path.join(paths.release, paths.dest)));
+});
+
+gulp.task('copy-desc', function () {
+  return gulp.src(['./package.json', './README.md', './requirements.txt'])
+    .pipe(gulp.dest(paths.release));
+})
