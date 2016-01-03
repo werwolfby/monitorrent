f1d191271508c5db9c8e68e6f1730ede65a822b9
Dzmitry Safarau <DSilencea@gmail.com>
2016-01-03 14:04:45 +0300

    Fixed #33: Rutor.org has moved. Changed rutor.org to rutor.info.

--- monitorrent/tests/test_rutortrackerplugin.py
+++ monitorrent/tests/test_rutortrackerplugin.py
@@ -8,16 +8,16 @@ import datetime
 class RutorTrackerPluginTest(DbTestCase):
     def test_can_parse_url(self):
         tracker = RutorOrgPlugin()
-        self.assertTrue(tracker.can_parse_url('http://rutor.org/torrent/442959'))
-        self.assertTrue(tracker.can_parse_url('http://www.rutor.org/torrent/442959'))
-        self.assertTrue(tracker.can_parse_url('http://d.rutor.org/torrent/442959'))
+        self.assertTrue(tracker.can_parse_url('http://rutor.info/torrent/442959'))
+        self.assertTrue(tracker.can_parse_url('http://www.rutor.info/torrent/442959'))
+        self.assertTrue(tracker.can_parse_url('http://d.rutor.info/torrent/442959'))
 
     @use_vcr
     def test_parse_url(self):
         tracker = RutorOrgPlugin()
-        original_name = u'Рэй Донован / Ray Donovan [03х01-05 из 12] (2015) HDTVRip 720р | NewStudio'
-        urls = ['http://rutor.org/torrent/442959',
-                'http://www.rutor.org/torrent/442959']
+        original_name = u'Время приключений с Финном и Джейком / Adventure Time with Finn & Jake [S01-06] (2010-2015) WEB-DL 720p | Cartoon Network, Зебуро'
+        urls = ['http://rutor.info/torrent/466037',
+                'http://www.rutor.info/torrent/466037']
         for url in urls:
             result = tracker.parse_url(url)
             self.assertIsNotNone(result, 'Can\'t parse url={}'.format(url))
@@ -26,18 +26,18 @@ class RutorTrackerPluginTest(DbTestCase):
 
     def test_parse_url_with_full_cover(self):
         tracker = RutorOrgPlugin()
-        urls = ['http://www.notrutor.org/torrent/442959',
-                'http://www.rutor.org/not-match-url/442959',
-                'http://rutor.org/search/']
+        urls = ['http://www.notrutor.info/torrent/442959',
+                'http://www.rutor.info/not-match-url/442959',
+                'http://rutor.info/search/']
         for url in urls:
             self.assertIsNone(tracker.parse_url(url))
 
     def test_prepare_request(self):
         tracker = RutorOrgPlugin()
-        urls = ['http://rutor.org/torrent/442959',
-                'http://www.rutor.org/torrent/442959',
-                'http://rutor.org/torrent/442959/rjej-donovan_ray-donovan-03h01-04-iz-12-2015-hdtvrip-720r-newstudio',
-                'http://www.rutor.org/torrent/442959/rjej-donovan_ray-donovan-03h01-04-iz-12-2015-hdtvrip-720r-newstud']
+        urls = ['http://rutor.info/torrent/442959',
+                'http://www.rutor.info/torrent/442959',
+                'http://rutor.info/torrent/442959/rjej-donovan_ray-donovan-03h01-04-iz-12-2015-hdtvrip-720r-newstudio',
+                'http://www.rutor.info/torrent/442959/rjej-donovan_ray-donovan-03h01-04-iz-12-2015-hdtvrip-720r-newstud']
         for url in urls:
             topic = RutorOrgTopic(url=url)
-            self.assertEqual('http://d.rutor.org/download/442959', tracker._prepare_request(topic))
+            self.assertEqual('http://d.rutor.info/download/442959', tracker._prepare_request(topic))
