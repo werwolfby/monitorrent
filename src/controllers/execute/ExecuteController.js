0421e4266d959f779510fd5c6b6603b78fa4fb14
werwolf.by <werwolf.by@gmail.com>
2016-02-29 04:59:20 +0300

    refactor: Extract execute subscription into ExecuteService.

--- src/controllers/execute/ExecuteController.js
+++ src/controllers/execute/ExecuteController.js
@@ -1,54 +1,15 @@
 app.controller('ExecuteController', function ($scope, $http, $q, mtToastService, ExecuteService) {
     $scope.messages = [];
-    var canceller = $q.defer();
 
     var started = function (message) {
         $scope.messages = [];
     };
 
     var finished = function (message) {
-        $scope.last_execute = message;
     };
 
     var log = function (message) {
         $scope.messages.push(message);
-    };
-
-    var destroyed = false;
-    var execute_id = null;
-    var log_id = 0;
-    
-    var processEvents = function (data) {
-        var result = data.data;
-        if (!result.is_running) {
-            executeListener();
-            return;
-        }
-        for (var i = 0; i < result.logs.length; i++) {
-            var evt = result.logs[i];
-            execute_id = evt.execute_id;
-            log_id = evt.id;
-            log(evt);
-        }
-        if (result.logs.length > 0) {
-            finished(result.logs[result.logs.length - 1].time);
-        }
-        if (result.is_running){
-            executeDetailsListener();
-        }
-    };
-
-    var executeListener = function () {
-        $http.get('api/execute/logs/current', {timeout: canceller.promise}).then(function (data) {
-            if (data.data.is_running) {
-                started();
-            }
-            processEvents(data);
-        });
-    };
-    
-    var executeDetailsListener = function () {
-        $http.get('/api/execute/logs/' + execute_id + '/details?after=' + log_id, {timeout: canceller.promise}).then(processEvents);
     };
 
     $scope.execute = function () {
@@ -60,13 +21,28 @@ app.controller('ExecuteController', function ($scope, $http, $q, mtToastService,
         ExecuteService.save($scope.interval);
     };
 
+    var executeStarted = started;
+    var executeEvents = function (logs) {
+        for (var i = 0; i < logs.length; i++) {
+            log(logs[i]);
+        }
+        if (logs.length > 0) {
+            $scope.last_execute = logs[logs.length - 1].time;
+        }
+    };
+    var executeFinished = finished;
+
+    var subscription = null;
+
     ExecuteService.load().then(function(data) {
         $scope.interval = data.data.interval;
         $scope.last_execute = data.data.last_execute;
-        executeListener();
+        subscription = ExecuteService.subscribe(executeStarted, executeEvents, executeFinished);
     });
 
     $scope.$on('$destroy', function() {
-        canceller.resolve();
+        if (subscription) {
+            subscription();
+        }
     });
 });
