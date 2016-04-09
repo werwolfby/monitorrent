45a1a6607e2c13dc91676b858656f209aa99df9b
werwolf.by <werwolf.by@gmail.com>
2016-04-10 01:08:52 +0300

    feat (logs): Add UI to update expire date for logs.

--- src/controllers/logs/LogsController.js
+++ src/controllers/logs/LogsController.js
@@ -1,29 +1,39 @@
-app.controller('LogsController', function ($scope, $http, $filter) {
-  $scope.executes = [];
+app.controller('LogsController', function ($scope, $http, $filter, mtToastService) {
+    $scope.executes = [];
 
-  $scope.getExecuteTitle = function (execute) {
-    var startTime = new Date(execute.start_time);
-    var finishTime = new Date(execute.finish_time);
-    var from = $filter('date')(startTime, 'yyyy/MM/dd HH:mm:ss');
-    var till = $filter('date')(finishTime, 'yyyy/MM/dd HH:mm:ss');
-    return "Execute from <i><b>" + from + "</b></i> till <i><b>" + till + "</b></i>";
-  };
+    $scope.getExecuteTitle = function (execute) {
+        var startTime = new Date(execute.start_time);
+        var finishTime = new Date(execute.finish_time);
+        var from = $filter('date')(startTime, 'yyyy/MM/dd HH:mm:ss');
+        var till = $filter('date')(finishTime, 'yyyy/MM/dd HH:mm:ss');
+        return "Execute from <i><b>" + from + "</b></i> till <i><b>" + till + "</b></i>";
+    };
 
-  var PAGE_SIZE = 10;
-  var last_page = 0;
-  var length = 0;
+    var PAGE_SIZE = 10;
+    var last_page = 0;
+    var length = 0;
 
-  function loadNextPage() {
-    if (length > 0 && last_page * PAGE_SIZE >= length)
-      return;
-    $http.get('/api/execute/logs?take=' + PAGE_SIZE + "&skip=" + (last_page * PAGE_SIZE)).then(function (result) {
-      for (var i = 0; i < result.data.data.length; i++) {
-        $scope.executes.push(result.data.data[i]);
-      }
-      length = result.data.count;
-    });
-    last_page++;
-  }
+    $scope.updateInterval = function (interval) {
+        $http.put('/api/settings/logs', {interval: interval})/*.then(function (data) {
+            mtToastService.show('Interval updated');
+        })*/;
+    }
 
-  $scope.loadNextPage = loadNextPage;
+    function loadNextPage() {
+        if (length > 0 && last_page * PAGE_SIZE >= length)
+            return;
+        $http.get('/api/execute/logs?take=' + PAGE_SIZE + "&skip=" + (last_page * PAGE_SIZE)).then(function (result) {
+            for (var i = 0; i < result.data.data.length; i++) {
+                $scope.executes.push(result.data.data[i]);
+            }
+            length = result.data.count;
+        });
+        last_page++;
+    }
+
+    $http.get('/api/settings/logs').then(function (data) {
+        $scope.interval = data.data.interval;
+    })
+
+    $scope.loadNextPage = loadNextPage;
 });
