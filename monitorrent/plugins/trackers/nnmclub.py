a4cb2a1b52a11e12e19dd2da1676d7d5ffdb89a5
Alexander Puzynia <werwolf.by@gmail.com>
2016-07-16 22:21:41 +0300

    feat(plugin): add nnmclub tracker class and few test to it

---
+++ monitorrent/plugins/trackers/nnmclub.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+from future import standard_library
+standard_library.install_aliases()
+from builtins import object
+import re
+from requests import Session
+import requests
+import urllib.request, urllib.parse, urllib.error
+from sqlalchemy import Column, Integer, String, ForeignKey
+from monitorrent.db import Base, DBSession
+from monitorrent.plugins import Topic
+from monitorrent.plugin_managers import register_plugin
+from monitorrent.utils.soup import get_soup
+from monitorrent.utils.bittorrent import Torrent
+from monitorrent.plugins.trackers import TrackerPluginBase, WithCredentialsMixin, ExecuteWithHashChangeMixin, LoginResult
+from urlparse import urlparse
+from urllib2 import unquote
+from phpserialize import loads
+
+PLUGIN_NAME = 'nnmclub.to'
+
+
+class NnmClubCredentials(Base):
+    __tablename__ = "nnmclub_credentials"
+
+    username = Column(String, primary_key=True)
+    password = Column(String, primary_key=True)
+    user_id = Column(String, nullable=True)
+    sid = Column(String, nullable=True)
+
+
+class NnmClubTopic(Topic):
+    __tablename__ = "nnmclub_topics"
+
+    id = Column(Integer, ForeignKey('topics.id'), primary_key=True)
+    download_id = Column(String, nullable=False)
+    hash = Column(String, nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': PLUGIN_NAME
+    }
+
+
+class NnmClubLoginFailedException(Exception):
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+
+class NnmClubTracker(object):
+    tracker_settings = None
+    tracker_domains = ['nnmclub.to', 'nnm-club.me']
+    title_headers = ['torrent :: nnm-club']
+    _login_url = 'http://nnm-club.me/forum/login.php'
+    _profile_page = "http://nnmclub.to/forum/profile.php?mode=viewprofile&u={}"
+
+    def __init__(self, user_id=None, sid=None):
+        self.user_id = user_id
+        self.sid = sid
+
+    def setup(self, user_id=None, sid=None):
+        self.user_id = user_id
+        self.sid = user_id
+
+    def can_parse_url(self, url):
+        parsed_url = urlparse(url)
+        return any([parsed_url.netloc == tracker_domain for tracker_domain in self.tracker_domains])
+
+    def parse_url(self, url):
+        url = self.get_url(url)
+        if not self.can_parse_url(url):
+            return None
+        parsed_url = urlparse(url)
+        if not parsed_url.path == '/forum/viewtopic.php':
+            return None
+
+        r = requests.get(url, allow_redirects=False, timeout=self.tracker_settings.requests_timeout)
+        if r.status_code != 200:
+            return None
+        soup = get_soup(r.text)
+        title = soup.title.string.strip()
+        for title_header in self.title_headers:
+            if title.lower().endswith(title_header):
+                title = title[:-len(title_header)].strip()
+                break
+
+        return self._get_title(title)
+
+    def login(self, username, password):
+        s = Session()
+        data = {"username": username, "password": password, "autologin": "on", "login": "%C2%F5%EE%E4"}
+        login_result = s.post(self._login_url, data, timeout=self.tracker_settings.requests_timeout)
+        if login_result.url.startswith(self._login_url):
+            # TODO get error info (although it shouldn't contain anything useful
+            raise NnmClubLoginFailedException(1, "Invalid login or password")
+        else:
+            sid = s.cookies['phpbb2mysql_4_sid']
+            data = s.cookies['phpbb2mysql_4_data']
+            parsed_data = loads(unquote(data))
+            self.user_id = parsed_data['userid']
+            self.sid = sid
+
+    def verify(self):
+        cookies = self.get_cookies()
+        if not cookies:
+            return False
+        profile_page_url = self._profile_page.format(self.user_id)
+        profile_page_result = requests.get(profile_page_url, cookies=cookies,
+                                           timeout=self.tracker_settings.requests_timeout)
+        return profile_page_result.url == profile_page_url
+
+    def get_cookies(self):
+        if not self.sid:
+            return False
+        return {'phpbb2mysql_4_sid': self.sid}
+
+    def get_download_url(self, url):
+        cookies = self.get_cookies()
+        page = requests.get(url, cookies=cookies, timeout=self.tracker_settings.requests_timeout)
+        page_soup = get_soup(page.text)
+        anchors = page_soup.find_all("a")
+        a = filter(lambda tag: tag.has_attr('href') and tag.attrs['href'].startswith("download.php?id="), anchors)[0]
+        download_url = 'http://' + self.tracker_domains[0] + '/forum/' + a.attrs['href']
+        return download_url
+
+    def get_url(self, url):
+        if not self.can_parse_url(url):
+            return False
+        parsed_url = urlparse(url)
+        parsed_url = parsed_url._replace(netloc=self.tracker_domains[0])
+        return parsed_url.geturl()
+
+    @staticmethod
+    def _get_title(title):
+        return {'original_name': title}
---
+++ monitorrent/tests/plugins/trackers/tests_nnmclub/test_nnmclubtracker.py
@@ -0,0 +1,71 @@
+# coding=utf-8
+from monitorrent.plugins.trackers import TrackerSettings
+from monitorrent.plugins.trackers.nnmclub import NnmClubTracker, LoginResult, NnmClubLoginFailedException
+from unittest import TestCase
+from monitorrent.tests import use_vcr
+from monitorrent.tests.plugins.trackers.tests_nnmclub.nnmclub_helper import NnmClubTrackerHelper
+
+helper = NnmClubTrackerHelper.login('login@gmail.com', 'p@$$w0rd')
+#helper = NnmClubTrackerHelper()
+
+
+class NnmClubTrackerTest(TestCase):
+    def setUp(self):
+        super(NnmClubTrackerTest, self).setUp()
+        self.tracker_settings = TrackerSettings(10)
+        self.tracker = NnmClubTracker()
+        self.tracker.tracker_settings = self.tracker_settings
+
+    def test_can_parse_url(self):
+        self.assertTrue(self.tracker.can_parse_url('http://nnmclub.to/forum/viewtopic.php?t=409969'))
+        self.assertTrue(self.tracker.can_parse_url('http://nnm-club.me/forum/viewtopic.php?t=409969'))
+        self.assertFalse(self.tracker.can_parse_url('http://not-nnm-club.me/forum/viewtopic.php?t=409969'))
+
+    def test_get_url(self):
+        expected = 'http://nnmclub.to/forum/viewtopic.php?t=409969'
+        self.assertEqual(self.tracker.get_url('http://nnmclub.to/forum/viewtopic.php?t=409969'), expected)
+        self.assertEqual(self.tracker.get_url('http://nnm-club.me/forum/viewtopic.php?t=409969'), expected)
+
+    @use_vcr
+    def test_parse_url(self):
+        original_name = u'Легенда о Тиле (1976) DVDRip'
+        urls = ['http://nnmclub.to/forum/viewtopic.php?t=409969',
+                'http://nnm-club.me/forum/viewtopic.php?t=409969']
+        for url in urls:
+            result = self.tracker.parse_url(url)
+            self.assertIsNotNone(result, 'Can\'t parse url={}'.format(url))
+            self.assertTrue('original_name' in result, 'Can\'t find original_name for url={}'.format(url))
+            self.assertEqual(original_name, result['original_name'])
+
+    @helper.use_vcr(inject_cassette=True)
+    def test_login(self, cassette):
+        # login will update cassette
+        check_sid = len(cassette) > 0
+        self.tracker.login(helper.real_username, helper.real_password)
+        if check_sid:
+            self.assertTrue((self.tracker.sid == helper.real_sid) or (self.tracker.sid == helper.fake_sid))
+        self.assertTrue((self.tracker.user_id == helper.real_user_id) or (self.tracker.user_id == helper.fake_user_id))
+
+    @use_vcr()
+    def test_fail_login(self):
+        with self.assertRaises(NnmClubLoginFailedException) as cm:
+            self.tracker.login("admin@nnmclub.to", "FAKE_PASSWORD")
+        self.assertEqual(cm.exception.code, 1)
+        self.assertEqual(cm.exception.message, u'Invalid login or password')
+
+    @helper.use_vcr(inject_cassette=True)
+    def test_verify(self, cassette):
+        user_id = helper.fake_user_id if len(cassette) > 0 else helper.real_user_id
+        sid = helper.fake_sid if len(cassette) > 0 else helper.real_sid
+        tracker = NnmClubTracker(user_id, sid)
+        tracker.tracker_settings = self.tracker_settings
+        self.assertTrue(tracker.verify())
+
+    def test_verify_false(self):
+        self.assertFalse(self.tracker.verify())
+
+    @use_vcr()
+    def test_verify_fail(self):
+        tracker = NnmClubTracker("9876543", '2' * 32)
+        tracker.tracker_settings = self.tracker_settings
+        self.assertFalse(tracker.verify())
