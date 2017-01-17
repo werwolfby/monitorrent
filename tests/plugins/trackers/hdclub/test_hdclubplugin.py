e6022f3a667be81e25578490feb368c033d2a21f
Alexander Puzynia <werwolf.by@gmail.com>
2017-01-17 22:50:33 +0300

    feat(plugin/tracker/hdclub): add support for hdclub.org over passkey

---
+++ tests/plugins/trackers/hdclub/test_hdclubplugin.py
@@ -0,0 +1,82 @@
+# coding=utf-8
+import six
+import pytest
+from mock import Mock, patch
+from monitorrent.plugins.trackers.hdclub import HdclubPlugin, HdclubTopic
+from tests import use_vcr, DbTestCase
+from tests.plugins.trackers import TrackerSettingsMock
+
+
+class TestHdclubPlugin(DbTestCase):
+    def setUp(self):
+        super(TestHdclubPlugin, self).setUp()
+
+        self.tracker_settings = TrackerSettingsMock(10, None)
+        self.plugin = HdclubPlugin()
+        self.plugin.init(self.tracker_settings)
+
+    def test_can_parse_url_success(self):
+        urls = [
+            'http://hdclub.org/details.php?id=20000',
+            'https://hdclub.org/details.php?id=20000',
+        ]
+
+        for url in urls:
+            assert self.plugin.can_parse_url(url)
+
+    def test_cannot_parse_url_success(self):
+        urls = [
+            'http://hdclub.com/details.php?id=20000',
+            'https://not.hdclub.org/details.php?id=20000',
+        ]
+
+        for url in urls:
+            assert not self.plugin.can_parse_url(url)
+
+    @use_vcr
+    def test_parse_url_success(self):
+        urls = [
+            'http://hdclub.org/details.php?id=20000',
+            'https://hdclub.org/details.php?id=20000',
+        ]
+
+        display_name = u'Донни Браско / Donnie Brasco (1997) [Extended Cut] 1080p BD-Remux'
+        for url in urls:
+            assert self.plugin.parse_url(url) == {'display_name': display_name}
+
+    def test_execute_success(self):
+        topics = [HdclubTopic()]
+        engine = Mock()
+        self.plugin.update_credentials({'passkey': '12345678'})
+
+        with patch("monitorrent.plugins.trackers.ExecuteWithHashChangeMixin.execute") as execute_mock:
+            self.plugin.execute(topics, engine)
+
+            execute_mock.assert_called_once_with(topics, engine)
+
+    def test_execute_failed(self):
+        topics = [HdclubTopic()]
+        engine = Mock()
+
+        with patch("monitorrent.plugins.trackers.ExecuteWithHashChangeMixin.execute") as execute_mock:
+            self.plugin.execute(topics, engine)
+
+            execute_mock.assert_not_called()
+
+    def test_execute_failed_2(self):
+        topics = [HdclubTopic()]
+        engine = Mock()
+        self.plugin.update_credentials({'passkey': ''})
+
+        with patch("monitorrent.plugins.trackers.ExecuteWithHashChangeMixin.execute") as execute_mock:
+            self.plugin.execute(topics, engine)
+
+            execute_mock.assert_not_called()
+
+    def test_prepare_request_success(self):
+        topic = HdclubTopic()
+        with patch("monitorrent.plugins.trackers.hdclub.HdclubTracker.get_download_url") as get_download_url_mock:
+            get_download_url_mock.return_value = 'http://hdclub.org/download.php?id=1'
+            download_url = self.plugin._prepare_request(topic)
+
+            assert download_url == 'http://hdclub.org/download.php?id=1'
