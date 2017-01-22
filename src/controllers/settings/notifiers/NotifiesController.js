dae6aa5eda51a93e81afa40b6d7453bf03f397bc
Alexander Puzynia <werwolf.by@gmail.com>
2017-01-22 14:02:09 +0300

    feat(ui/notifications): work with backend to set/unset notify on checkboxes

--- src/controllers/settings/notifiers/NotifiesController.js
+++ src/controllers/settings/notifiers/NotifiesController.js
@@ -8,12 +8,30 @@ app.controller('NotifiersController', function ($scope, NotifiersService, mtToas
         });
     });
 
+    NotifiersService.get_notify_on().then(function (data) {
+        var levels = data.data;
+
+        $scope.notify_on_download = levels.indexOf('DOWNLOAD') >= 0;
+        $scope.notify_on_error = levels.indexOf('ERROR') >= 0;
+        $scope.notify_on_status_changed = levels.indexOf('STATUS_CHANGED') >= 0;
+
+        $scope.levels = levels;
+    });
+
     $scope.path = $location.path();
     $scope.set_enabled = function(notifier){
         NotifiersService.set_enabled(notifier.name, notifier.enabled);
     };
 
-    $scope.set_notify_on = function (notify, value) {
+    $scope.set_notify_on = function(field, value) {
+        var idx = $scope.levels.indexOf(field);
+        if (idx >= 0 && !value) {
+            $scope.levels.splice(idx, 1);
+        } else {
+            $scope.levels.push(field);
+        }
+
+        NotifiersService.set_notify_on($scope.levels);
     };
 
     $scope.listClicked=function(ev){
