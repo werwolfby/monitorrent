c99e10d684a2a1a59540c35cf4b42663712c05a2
Alexander Puzynia <werwolf.by@gmail.com>
2017-02-07 01:41:09 +0300

    feat(ui): apply default theme

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,13 @@ import 'vue-material/dist/vue-material.css'
 Vue.use(Router)
 Vue.use(VueMaterial)
 
+Vue.material.registerTheme('default', {
+  primary: 'blue-grey',
+  accent: 'deep-purple',
+  warn: 'deep-orange',
+  background: 'white'
+})
+
 export default new Router({
   routes: [
     {
