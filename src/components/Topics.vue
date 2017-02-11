8ebc4ea4e280de1ce3065067a729f0811cefd7a0
Alexander Puzynia <werwolf.by@gmail.com>
2017-02-11 14:43:54 +0300

    feat(ui): add TopicsExecute header

--- src/components/Topics.vue
+++ src/components/Topics.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <mt-topics-execute></mt-topics-execute>
     <mt-topics-header></mt-topics-header>
     <mt-topics-list></mt-topics-list>
   </div>
@@ -8,12 +9,14 @@
 <script>
 import TopicsList from './TopicsList'
 import TopicsHeader from './TopicsHeader'
+import TopicsExecute from './TopicsExecute'
 
 export default {
   name: 'Topics',
   components: {
     'mt-topics-list': TopicsList,
-    'mt-topics-header': TopicsHeader
+    'mt-topics-header': TopicsHeader,
+    'mt-topics-execute': TopicsExecute
   }
 }
 </script>
---
+++ src/components/TopicsExecute.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <md-layout md-row md-gutter="24" class="mt-topics-header">
+      <md-layout md-flex>
+        <h4 md-flex class="mt-subheader mt-executing">
+          <span class="mt-bold">Last Executed</span>&nbsp;at {{execute.finish_time}} ({{relative_execute}})
+        </h4>
+      </md-layout>
+      <md-button class="md-icon-button" style="margin: auto 12px">
+        <md-icon>input</md-icon>
+      </md-button>
+    </md-layout>
+    <md-divider></md-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      'execute': {
+        'finish_time': '13:52'
+      },
+      'relative_execute': '5 seconds ago'
+    }
+  },
+  name: 'TopicsExecute'
+}
+</script>
+
+<style scoped>
+.mt-topics-header {
+  padding: 0px 16px;
+}
+
+.mt-executing {
+  text-overflow: ellipsis;
+  overflow: hidden !important;
+  white-space: nowrap;
+}
+
+.mt-bold {
+  font-weight: 600;
+}
+
+.mt-subheader {
+font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0.010em;
+  line-height: 24px;
+}
+</style>
