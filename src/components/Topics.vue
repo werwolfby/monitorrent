55b6986c96737c88faef86976483103adf53bebf
Alexander Puzynia <werwolf.by@gmail.com>
2017-02-11 14:22:04 +0300

    feat(ui): add TopicsHeader component, and rename Topics to TopicsList component

--- src/components/Topics.vue
+++ src/components/Topics.vue
@@ -1,54 +1,19 @@
 <template>
-  <md-list class="md-double-line" style="padding: 0px">
-    <md-list-item v-for="topic in topics">
-      <md-avatar>
-        <a :href="topic.url" target="_blank" hide-gt-xs>
-          <img v-bind:src="'static/images/' + topic.tracker + '.png'"/>
-        </a>
-      </md-avatar>
-
-      <div class="md-list-text-container">
-        <span>{{topic.display_name}}</span>
-        <span>Last update: {{topic.last_update}}</span>
-      </div>
-
-      <md-menu class="md-list-action" md-direction="bottom left" md-size="4">
-        <md-button md-menu-trigger class="md-icon-button">
-          <md-icon>more_vert</md-icon>
-        </md-button>
-        <md-menu-content md-size="4">
-          <md-menu-item>
-            <md-icon>edit</md-icon><span>Edit</span>
-          </md-menu-item>
-          <md-menu-item>
-            <md-icon>pause</md-icon><span>Pause</span>
-          </md-menu-item>
-          <md-menu-item>
-            <md-icon>restore</md-icon><span>Reset Status</span>
-          </md-menu-item>
-          <md-menu-item>
-            <md-icon>input</md-icon><span>Execute</span>
-          </md-menu-item>
-        </md-menu-content>
-      </md-menu>
-    </md-list-item>
-  </md-list>
+  <div>
+    <mt-topics-header></mt-topics-header>
+    <mt-topics-list></mt-topics-list>
+  </div>
 </template>
 
 <script>
+import TopicsList from './TopicsList'
+import TopicsHeader from './TopicsHeader'
+
 export default {
-  data: () => {
-    return {
-      'topics': []
-    }
-  },
-  name: 'topics',
-  mounted () {
-    fetch('/api/topics')
-    .then(data => data.json())
-    .then(data => {
-      this.topics = data
-    })
+  name: 'Topics',
+  components: {
+    'mt-topics-list': TopicsList,
+    'mt-topics-header': TopicsHeader
   }
 }
 </script>
---
+++ src/components/TopicsHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <md-layout md-row md-gutter="24" class="mt-topics-header">
+      <md-layout md-flex>
+        <md-input-container>
+          <label>Filter</label>
+          <md-input/>
+        </md-input-container>
+      </md-layout>
+
+      <div>
+        <md-input-container>
+          <label>Sort</label>
+          <md-select v-model="order">
+            <md-option value="display_name">Name</md-option>
+            <md-option value="-last_update">Last Update</md-option>
+          </md-select>
+        </md-input-container>
+      </div>
+
+      <md-button class="md-icon-button" style="margin: auto 12px">
+        <md-icon>add</md-icon>
+      </md-button>
+    </md-layout>
+    <md-divider></md-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {
+      order: '-last_update'
+    }
+  },
+  name: 'TopicsHeader'
+}
+</script>
+
+<style scoped>
+.mt-topics-header {
+  padding: 0px 16px;
+}
+</style>
