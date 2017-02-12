d953fe2d100686ef503bed52ec4b6b7276e50dcb
Alexander Puzynia <werwolf.by@gmail.com>
2017-02-12 11:53:22 +0300

    chore(webpack): add Promise polyfill, to support old browsers and tests

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -69,5 +70,10 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'Promise': 'es6-promise'
+    })
+  ]
 }
