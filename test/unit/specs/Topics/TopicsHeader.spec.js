9060ffb20940a4846ffd163e391ee1189db91513
Alexander Puzynia <werwolf.by@gmail.com>
2017-02-12 19:22:14 +0300

    test(core): complete tests for TopicsHeader

--- test/unit/specs/Topics/TopicsHeader.spec.js
+++ test/unit/specs/Topics/TopicsHeader.spec.js
@@ -2,9 +2,28 @@ import Vue from 'vue'
 import TopicsHeader from 'src/components/Topics/TopicsHeader'
 
 describe('TopicsHeader.vue', () => {
+  const Constructor = Vue.extend(TopicsHeader)
+
   it('should render correct content', () => {
-    const Contructor = Vue.extend(TopicsHeader)
-    const vm = new Contructor().$mount()
+    const vm = new Constructor().$mount()
     expect(vm.$el.querySelector('#filter')).to.not.be.null
+    expect(vm.$el.querySelector('#order')).to.not.be.null
+    expect(vm.$el.querySelector('#add')).to.not.be.null
+  })
+
+  it('should have empty values by default', () => {
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('#filter')).to.have.property('value').equal('')
+    expect(vm.$el.querySelector('#order')).to.have.property('value').equal('')
+  })
+
+  it('should have values equal to props', async () => {
+    const props = { filter: 'lostfilm.tv', order: '-last_update' }
+    const vm = new Constructor({ propsData: props }).$mount()
+
+    await Vue.nextTick()
+
+    expect(vm.$el.querySelector('#filter')).to.have.property('value').equal('lostfilm.tv')
+    expect(vm.$el.querySelector('#order')).to.have.property('value').equal('-last_update')
   })
 })
