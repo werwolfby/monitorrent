b3f2c34a50ec24d9d7772018f545e50478bd5ba5
Alexander Puzynia <werwolf.by@gmail.com>
2017-02-18 22:58:31 +0300

    feat(front-end/topics): if there are no any topics in list display "Add torrent and press execute"

--- test/unit/specs/Topics/TopicsList.spec.js
+++ test/unit/specs/Topics/TopicsList.spec.js
@@ -10,6 +10,7 @@ describe('TopicsList.vue', () => {
         expect(vm.$el.textContent).to.contain('Loading...')
 
         expect(vm.$refs.list).to.be.not.ok
+        expect(vm.$refs.addTopics).to.be.not.ok
         expect(vm.$refs.loading).to.be.ok
         expect(vm.$refs.loading.$el.textContent).to.contain('Loading...')
     })
@@ -21,6 +22,7 @@ describe('TopicsList.vue', () => {
         expect(vm.$el.textContent).to.contain('Loading...')
 
         expect(vm.$refs.list).to.be.not.ok
+        expect(vm.$refs.addTopics).to.be.not.ok
         expect(vm.$refs.loading).to.be.ok
         expect(vm.$refs.loading.$el.textContent).to.contain('Loading...')
     })
@@ -29,8 +31,8 @@ describe('TopicsList.vue', () => {
         const propsData = {
             loading: true,
             topics: [
-            { display_name: 'Topic 1 / Season 1', last_update: null },
-            { display_name: 'Topic 1 / Season 1', last_update: null }
+                { display_name: 'Topic 1 / Season 1', last_update: null },
+                { display_name: 'Topic 1 / Season 1', last_update: null }
             ]
         }
         const vm = new Constructor({ propsData }).$mount()
@@ -38,7 +40,9 @@ describe('TopicsList.vue', () => {
         expect(vm.$el.textContent).to.contain('Loading...')
 
         expect(vm.$refs.list).to.be.not.ok
+        expect(vm.$refs.addTopics).to.be.not.ok
         expect(vm.$refs.loading).to.be.ok
+
         expect(vm.$refs.loading.$el.textContent).to.contain('Loading...')
     })
 
@@ -46,18 +50,47 @@ describe('TopicsList.vue', () => {
         const propsData = {
             loading: false,
             topics: [
-            { display_name: 'Topic 1 / Season 1', last_update: null, tracker: 'lostfilm.tv' },
-            { display_name: 'Topic 2 / Season 2-3', last_update: null, tracker: 'rutor.org' }
+                { display_name: 'Topic 1 / Season 1', last_update: null, tracker: 'lostfilm.tv' },
+                { display_name: 'Topic 2 / Season 2-3', last_update: null, tracker: 'rutor.org' }
             ]
         }
         const vm = new Constructor({ propsData }).$mount()
 
         expect(vm.$refs.list).to.be.ok
+        expect(vm.$refs.addTopics).to.be.not.ok
         expect(vm.$refs.loading).to.be.not.ok
 
         expect(vm.$refs.topic).to.have.lengthOf(2)
 
         expect(vm.$refs.topic[0].$el.textContent).to.contain(propsData.topics[0].display_name)
         expect(vm.$refs.topic[1].$el.textContent).to.contain(propsData.topics[1].display_name)
+    })
+
+    it('should display "add topics and press execute" with loading = false and empty topics array', async () => {
+        const propsData = {
+            loading: false,
+            topics: []
+        }
+        const vm = new Constructor({ propsData }).$mount()
+
+        expect(vm.$refs.loading).to.be.not.ok
+        expect(vm.$refs.list).to.be.not.ok
+        expect(vm.$refs.addTopics).to.be.ok
+
+        expect(vm.$refs.addTopics.$el.textContent).to.contain('Add torrent and press execute')
+    })
+
+    it('should display "add topics and press execute" with loading = false and null topics array', async () => {
+        const propsData = {
+            loading: false,
+            topics: null
+        }
+        const vm = new Constructor({ propsData }).$mount()
+
+        expect(vm.$refs.loading).to.be.not.ok
+        expect(vm.$refs.list).to.be.not.ok
+        expect(vm.$refs.addTopics).to.be.ok
+
+        expect(vm.$refs.addTopics.$el.textContent).to.contain('Add torrent and press execute')
     })
 })
