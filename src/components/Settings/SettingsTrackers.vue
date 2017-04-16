d1db3d79f725faf123d967901f7afa650539e551
Alexander Puzynia <werwolf.by@gmail.com>
2017-04-17 02:25:34 +0300

    feat(ui/settings/trackers): show trackers list

--- src/components/Settings/SettingsTrackers.vue
+++ src/components/Settings/SettingsTrackers.vue
@@ -1,16 +1,41 @@
 <template>
     <div>
         <mt-route-toolbar title="Trackers"></mt-route-toolbar>
+        <md-divider></md-divider>
+        <md-list ref="list" style="padding: 0px">
+            <md-list-item v-for="t of trackers">
+                <md-avatar>
+                    <img :src="`static/images/${t.name}.png`" :alt="t.name">
+                </md-avatar>
+                <span>{{t.name}}</span>
+            </md-list-item>
+        </md-list>
     </div>
 </template>
 
 <script>
 import RouteToolbar from './RouteToolbar'
+import { mapActions, mapState } from 'vuex'
+import _ from 'lodash'
 
 export default {
+    computed: {
+        ...mapState({
+            loading: state => state.trackers.loading,
+            trackers: state => _.sortBy(state.trackers.trackers, 'name')
+        })
+    },
     components: {
         'mt-route-toolbar': RouteToolbar
     },
-    name: 'SettingsTrackers'
+    name: 'SettingsTrackers',
+    created () {
+        this.loadTrackers()
+    },
+    methods: {
+        ...mapActions({
+            loadTrackers: 'loadTrackers'
+        })
+    }
 }
 </script>
