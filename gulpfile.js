193e76eafa3ffecf44990db69017618510ae7463
Alexander Puzynia <werwolf.by@gmail.com>
2021-09-26 22:36:40 -0700

    feat: update gulpfile.js to support gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-var path = require('path');
-var del = require('del');
+const path = require('path');
+const del = require('del');
 
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var less = require('gulp-less');
-var preprocess = require('gulp-preprocess');
-var zip = require('gulp-zip');
-var rename = require('gulp-rename');
-var ngAnnotate = require('gulp-ng-annotate');
-var uglify = require('gulp-uglify');
-var git = require('git-rev');
-var htmlmin = require('gulp-htmlmin');
-var templateCache = require('gulp-angular-templatecache');
-var addStream = require('add-stream');
+const gulp = require('gulp');
+const jshint = require('gulp-jshint');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const less = require('gulp-less');
+const preprocess = require('gulp-preprocess');
+const zip = require('gulp-zip');
+const rename = require('gulp-rename');
+const ngAnnotate = require('gulp-ng-annotate');
+const uglify = require('gulp-uglify');
+const git = require('git-rev');
+const htmlmin = require('gulp-htmlmin');
+const templateCache = require('gulp-angular-templatecache');
+const addStream = require('add-stream');
 
-var pkg = require('./package.json');
+const pkg = require('./package.json');
 
-var paths = {
+const paths = {
   scripts: ['./src/**/*.js'],
   index_pages: ['./src/*.html'],
   templates: ['./src/**/*.html', '!./src/*.html'],
@@ -28,14 +28,12 @@ var paths = {
   release: 'dist'
 };
 
-gulp.task('default', ['clean', 'jshint', 'concat', 'copy', 'less', 'copy-index']);
-
 gulp.task('clean', function () {
-  return del.sync([paths.dest]);
+  return del([paths.dest]);
 });
 
 gulp.task('clean-release', function () {
-  return del.sync([paths.release]);
+  return del([paths.release]);
 });
 
 gulp.task('jshint', function () {
@@ -84,17 +82,6 @@ gulp.task('less', function () {
     .pipe(gulp.dest(path.join(paths.dest, 'styles')));
 });
 
-gulp.task('copy-index', ['copy-index-html', 'copy-login-html']);
-
-function preprocessIndexHtmlTpl(mode, rev) {
-  return gulp.src(['./src/index.html'])
-    .pipe(preprocess({
-      context: { VERSION: pkg.version, MODE: mode, COMMIT_HASH: rev }
-    }))
-    .pipe(rename(mode + '.html'))
-    .pipe(gulp.dest(paths.dest));
-}
-
 gulp.task('copy-index-html', function (cb) {
   git.long(function (rev) {
     var stream = preprocessIndexHtmlTpl('index', rev);
@@ -109,32 +96,45 @@ gulp.task('copy-login-html', function (cb) {
   });
 });
 
-gulp.task('watch', ['default'], function () {
+gulp.task('copy-index', gulp.parallel('copy-index-html', 'copy-login-html'));
+
+gulp.task('default', gulp.series('clean', 'jshint', 'concat', 'copy', 'less', 'copy-index'));
+
+function preprocessIndexHtmlTpl(mode, rev) {
+  return gulp.src(['./src/index.html'])
+    .pipe(preprocess({
+      context: { VERSION: pkg.version, MODE: mode, COMMIT_HASH: rev }
+    }))
+    .pipe(rename(mode + '.html'))
+    .pipe(gulp.dest(paths.dest));
+}
+
+gulp.task('watch', gulp.series('default', function () {
   gulp.watch(paths.statics,                         ['copy']);
   gulp.watch(paths.scripts.concat(paths.templates), ['concat']);
   gulp.watch(paths.styles,                          ['less']);
   gulp.watch(paths.index_pages,                     ['copy-index']);
-});
-
-gulp.task('release', ['dist'], function () {
-  return gulp.src([paths.release + '/**/*.*'])
-    .pipe(zip(pkg.name + '-' + pkg.version + '.zip'))
-    .pipe(gulp.dest('.'));
-});
-
-gulp.task('dist', ['clean-release', 'copy-python', 'copy-webapp', 'copy-desc']);
+}));
 
 gulp.task('copy-python', function () {
   return gulp.src(['./**/*.py', '!./tests*/**/*.*', '!./venv/**/*.*', '!./' + paths.release + '/**/*.py'])
     .pipe(gulp.dest(paths.release));
 });
 
-gulp.task('copy-webapp', ['default'], function () {
+gulp.task('copy-webapp', gulp.series('default', function () {
   return gulp.src([path.join(paths.dest, '**/*.*'), '!./**/*.map'])
     .pipe(gulp.dest(path.join(paths.release, paths.dest)));
-});
+}));
 
 gulp.task('copy-desc', function () {
   return gulp.src(['./package.json', './README.md', './requirements.txt'])
     .pipe(gulp.dest(paths.release));
 })
+
+gulp.task('dist', gulp.series('clean-release', gulp.parallel('copy-python', 'copy-webapp', 'copy-desc')));
+
+gulp.task('release', gulp.series('dist', function () {
+  return gulp.src([paths.release + '/**/*.*'])
+    .pipe(zip(pkg.name + '-' + pkg.version + '.zip'))
+    .pipe(gulp.dest('.'));
+}));
