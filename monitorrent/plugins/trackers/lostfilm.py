37d515b7ce07310fdc25885ef1020fca1b1ac9f0
Alexander Puzynia <werwolf.by@gmail.com>
2023-04-06 00:16:30 -0700

    fix: add lostfilm topic without creds

--- monitorrent/plugins/trackers/lostfilm.py
+++ monitorrent/plugins/trackers/lostfilm.py
@@ -754,8 +754,8 @@ class LostFilmPlugin(WithCredentialsMixin, TrackerPluginBase):
             quality = cred.default_quality if cred else 'SD'
             session = cred.session if cred else None
             domain = cred.domain if cred else 'www.lostfilm.tv'
-            cookies = json.loads(cred.cookies) if cred.cookies else None
-            headers = json.loads(cred.headers) if cred.headers else None
+            cookies = json.loads(cred.cookies) if cred and cred.cookies else None
+            headers = json.loads(cred.headers) if cred and cred.headers else None
         self.tracker.setup(session=session, headers=headers, cookies=cookies, domain=domain)
         parsed_url = self.tracker.parse_url(url)
         if parsed_url is None or isinstance(parsed_url, Response):
